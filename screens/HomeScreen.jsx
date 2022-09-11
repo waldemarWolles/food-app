9cde89afe63a03f041cbc63bc147f0fab60d47aa
Waldemar <chooter1999@gmail.com>
2022-09-11 21:24:38 +0300

    refactored code a little

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 export default function HomeScreen() {
+
+  const navigation = useNavigation()
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false
+    })
+
+
+  }, [])
+
   return (
-    <View className="flex-1 items-center justify-center">
+    <SafeAreaView className="flex-1 items-center justify-center">
       <Text className="text-fuchsia-700">HomeScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
